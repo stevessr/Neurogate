c5dcb81cc25c3dd6d59ab8cafadf079d24d6ced1
ryotairi <ryotairi.383e@extera.xyz>
2026-05-02 16:02:33 +0500

    feat: use proxy in google mozhi provider

--- src/translators/google_mozhi.ts
+++ src/translators/google_mozhi.ts
@@ -1,18 +1,22 @@
+import { fetch } from 'undici';
 import { TranslationResponse } from ".";
 import config from "../services/config";
 import logger from "../services/logger";
+import getProxy from "../services/proxy";
 import BaseTranslator from "./base";
 
 class MozhiGoogleTranslator extends BaseTranslator {
     name: string = 'mozhi-google';
-    
+
     async translateText(sourceLang: string, targetLang: string, text: string): Promise<TranslationResponse> {
         // TODO: sort by usage, aka load balancing
         const shuffledList = config.mozhiGoogleInstances.sort(() => Math.random() - 0.5);
 
         for (const mozhiInstance of shuffledList) {
             try {
-                const response = await fetch(`${mozhiInstance}/api/translate?engine=google&from=${sourceLang}&to=${targetLang}&text=${encodeURIComponent(text)}`);
+                const response = await fetch(`${mozhiInstance}/api/translate?engine=google&from=${sourceLang}&to=${targetLang}&text=${encodeURIComponent(text)}`, {
+                    dispatcher: getProxy()!,
+                });
                 if (!response.ok) throw new Error(`HTTP request failed with ${response.status} ${response.statusText}`);
                 const json: any = await response.json();
                 if (typeof json['translated-text'] !== 'string' || !json['translated-text']) throw new Error(`'translated-text' is not a string or it's empty`);
